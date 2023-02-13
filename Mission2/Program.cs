6a51d3f3d64b464be31c0152a8bc1ff2eee327cf
Tatyana <tatyana.kurii@mail.ru>
2023-02-13 15:18:12 +0600

    Mission2

--- Mission2/Program.cs
+++ Mission2/Program.cs
@@ -2,3 +2,15 @@
 // M = 1; N = 5 -> "1, 2, 3, 4, 5"
 // M = 4; N = 8 -> "4, 5, 6, 7, 8"
 
+void ShowNumbers(int start, int finish)
+{
+  if(start > finish) 
+  {
+    return;
+  }
+
+System.Console.Write($"{start} ");
+ShowNumbers(start+ 1, finish);
+}
+
+ShowNumbers(5, 10);
