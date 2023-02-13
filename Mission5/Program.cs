2c600e45b369755f16c7fc02e255d68e7400132b
Tatyana <tatyana.kurii@mail.ru>
2023-02-13 16:55:18 +0600

    Mission5

---
+++ Mission5/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 5: Определите, является ли число степенью двойки:
+// N = 16 -> "Является степень двойки"
+// N = 12 -> “Не является степенью двойки”
+
+bool Validate(int number)
+{
+  if (number == 1)
+  {
+    return true;
+  }
+  if (number % 2 == 1)
+  {
+    return false;
+  }
+  number = number / 2;
+  return Validate(number);
+}
+System.Console.WriteLine(Validate(12));
